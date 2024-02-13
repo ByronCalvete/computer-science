f4dc2e1980a6117a018739783ad2b4e881236f66
Byron J Calvete <calvete94@gmail.com>
2024-02-13 23:31:48 +0100

    add: exercise 3.1

---
+++ IntroCS/PY4E/conditional-execution/ex3_1.py
@@ -0,0 +1,16 @@
+# Rewrite you pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+
+hours = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+
+fh = float(hours)
+fr = float(rate)
+
+if fh > 40.0:
+  regPay = fh * fr
+  overtime = (fh - 40)*(fr * 0.5)
+  pay = regPay + overtime
+  print('Pay:', pay)
+else:
+  pay = fh * fr
+  print('Pay:', pay)
