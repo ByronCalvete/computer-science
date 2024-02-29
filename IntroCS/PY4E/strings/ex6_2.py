307b68d68d74879d8425bd8054a0b736adf45db2
Byron J Calvete <calvete94@gmail.com>
2024-02-29 22:04:33 +0100

    add: exercise 6.2

---
+++ IntroCS/PY4E/strings/ex6_2.py
@@ -0,0 +1,4 @@
+# Given that "fruit" is a string, what does "fruit[:]" mean?
+
+str = 'fruit'
+print(str[:]) # Print 'fruit', because this notiation "[:]" returns all the characters from start to end  
