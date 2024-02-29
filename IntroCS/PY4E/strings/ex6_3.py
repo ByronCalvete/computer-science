2dca9eeaf77de245ec372b8403549e51c27c81bd
Byron J Calvete <calvete94@gmail.com>
2024-02-29 22:35:40 +0100

    add: exercise 6.3

---
+++ IntroCS/PY4E/strings/ex6_3.py
@@ -0,0 +1,12 @@
+# Encapsulate this code in a function named "count" and generalize it so that it accepts the string and the letter as arguments
+
+def count(word, letter):
+  count = 0
+  for i in word:
+    if i == letter:
+      count = count + 1
+  
+  convert = str(count)
+  print('The letter ' + letter + ' appears ' + convert + ' times on word ' + word)
+
+count('banana', 'a') # The letter a appears 3 times on word banana
