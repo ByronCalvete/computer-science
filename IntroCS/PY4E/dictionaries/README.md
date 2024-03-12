10c7202f14e8e0d6b7b654d54d928f0bc567f965
Byron J Calvete <calvete94@gmail.com>
2024-03-12 23:57:18 +0100

    add: lecture 9: Dictionaries

--- IntroCS/PY4E/dictionaries/README.md
+++ IntroCS/PY4E/dictionaries/README.md
@@ -1,3 +1,12 @@
 # Dictionaries
 
 [Lecture](https://www.py4e.com/html3/09-dictionaries)
+
+In this lecture we talk about:
+- What is a collection
+- Lists versus dictionaries
+- Dictionary Constants
+- The most common word
+- Using the __get()__ method
+- Writing dictionary loops
+- Sneak peek: Tuples
---
+++ IntroCS/PY4E/dictionaries/test-lecture.py
@@ -0,0 +1,50 @@
+# counts = dict()
+
+# names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+
+# for name in names:
+#   counts[name] = counts.get(name, 0) + 1
+
+# print(counts)
+
+#### Counts the frequency of the words in a line of text
+
+# counts = dict()
+
+# line = input('Enter a line of text:') # the clown ran after the car and the car ran into the tend and the tend fell down on the clown and the car
+# words = line.split()
+
+# print('Words:', words)
+# print('Counting...')
+
+# for word in words:
+#   counts[word] = counts.get(word, 0) + 1
+
+# print('Counts', counts)
+
+#### Counts words in a text file
+
+name = input('Enter file: ')
+
+try:
+  fhand = open(name)
+except:
+  print('File cannot be openend', name)
+  exit()
+
+counts = dict()
+
+for line in fhand:
+  words = line.split()
+  for word in words:
+    counts[word] = counts.get(word, 0) + 1
+
+bigCount = None
+bigWord = None
+
+for word,count in counts.items():
+  if bigCount is None or count > bigCount:
+    bigWord = word
+    bigCount = count
+
+print(bigWord, bigCount)
