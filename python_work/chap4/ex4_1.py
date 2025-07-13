b617e8396a0890bd721070df2c3f189ef1041720
Byron J Calvete <calvete94@gmail.com>
2025-07-13 23:18:32 +0200

    add: Chapter 4, part2

---
+++ python_work/chap4/ex4_1.py
@@ -0,0 +1,6 @@
+pizzas = ['fugazzata', 'muzzarella', 'napolitana']
+
+for pizza in pizzas:
+  print(f'The delicious {pizza.title()} pizza')
+
+print("I really love the Argentina's style pizza!!")
---
+++ python_work/chap4/ex4_2.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'horse']
+
+for animal in animals:
+  print(f'A {animal.title()} is a good friend!')
+
+print('All of this animals are a good pet!')
