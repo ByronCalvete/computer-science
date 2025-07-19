4166a0a683faab836fc8dfba44b22bb6a7c6ad39
Byron J Calvete <calvete94@gmail.com>
2025-07-19 22:18:40 +0200

    add: Chapter 5, part1

---
+++ python_work/chap5/ex5_1.py
@@ -0,0 +1,19 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru') # True
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi') # False
+
+print('---')
+
+list_friends = ['camila', 'pedro', 'andrea', 'jose']
+print(f'The list of friend is: {list_friends}')
+
+friend = 'maria'
+print(f'If {friend.title()} in the list of friends?')
+print(friend in list_friends)
+
+friend = 'jose'
+print(f'\nIs {friend.title()} in the list of friends?')
+print(friend in list_friends)
